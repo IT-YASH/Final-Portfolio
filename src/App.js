e10ae72ffbf4a8fab7575e510893cb43db901176
IT-YASH <pyash7646@gmail.com>
2024-05-07 00:44:40 +0530

    update the reacts code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import htmlLogo from "../src/images/html-logo.png";
-// import cssLogo from "../src/images/css-logo.png";
-// import jsLogo from "../src/images/js-logo.png";
-// import reactLogo from "../src/images/react-logo.png";
+import htmlLogo from "../src/images/html.png";
+import cssLogo from "../src/images/css.png";
+import jsLogo from "../src/images/js.png";
+import reactLogo from "../src/images/react.png";
 import images from "../src/images/img.png";
 import "./App.css";
 
@@ -80,53 +80,53 @@ function App() {
 
       <section className="thirdsection">
         <div className="container">
-          <div className="box">
-            <img src={images} alt="HTML Logo" className="html-logo" />
+          <div className="images">
+            <div className="html-logo">
+              <img src={htmlLogo} alt="htmllogo" srcset="" />
+            </div>
+            <div className="css-logo">
+              <img src={cssLogo} alt="csslogo" srcset="" />
+            </div>
+            <div className="js-logo">
+              <img src={jsLogo} alt="jslogo" srcset="" />
+            </div>
+            <div className="react-logo">
+              <img src={reactLogo} alt="reactlogo" srcset="" />
+            </div>
           </div>
-          <div className="box">
-            <img src={images} alt="CSS Logo" className="css-logo" />
-          </div>
-          <div className="box">
-            <img src={images} alt="JavaScript Logo" className="js-logo" />
-          </div>
-          <div className="box">
-            <img src={images} alt="React Logo" className="react-logo" />
-          </div>
-        </div>
-        <div className="container">
-          <div className="box">
-            <img src={images} alt="HTML Logo" className="html-logo" />
-          </div>
-          <div className="box">
-            <img src={images} alt="CSS Logo" className="css-logo" />
-          </div>
-          <div className="box">
-            <img src={images} alt="JavaScript Logo" className="js-logo" />
-          </div>
-          <div className="box">
-            <img src={images} alt="React Logo" className="react-logo" />
+          <div className="images">
+            <div className="html-logo">
+              <img src={htmlLogo} alt="htmllogo" srcset="" />
+            </div>
+            <div className="css-logo">
+              <img src={cssLogo} alt="csslogo" srcset="" />
+            </div>
+            <div className="js-logo">
+              <img src={jsLogo} alt="jslogo" srcset="" />
+            </div>
+            <div className="react-logo">
+              <img src={reactLogo} alt="reactlogo" srcset="" />
+            </div>
           </div>
         </div>
       </section>
 
-      <hr />
-
       <footer>
-        <h3 className="footercentered-text">
-          FeelFree To <span className="introcolor">Contact Me</span>
-        </h3>
-        <div class="social-icons">
-          <a href="#">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-twitter"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </div>
-      </footer>
+  <h3 className="footercentered-text">
+    FeelFree To <span className="introcolor">Contact Me</span>
+  </h3>
+  <div class="social-icons">
+    <a href="https://www.facebook.com/yourprofile">
+      <i class="fab fa-facebook-f"></i>
+    </a>
+    <a href="https://twitter.com/yourprofile">
+      <i class="fab fa-twitter"></i>
+    </a>
+    <a href="https://www.instagram.com/yourprofile">
+      <i class="fab fa-instagram"></i>
+    </a>
+  </div>
+</footer>
     </>
   );
 }
