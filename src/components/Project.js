1ae878c3b96dfd2a7ebba07187c372669c28d642
IT-YASH <pyash7646@gmail.com>
2024-06-03 23:39:26 +0530

    updated the code

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,32 +1,32 @@
 import React from "react";
-import { FaExternalLinkAlt } from 'react-icons/fa';
-import '../Project.css';
+import { FaExternalLinkAlt } from "react-icons/fa";
+import "../Project.css";
 
 const projects = [
   {
     id: 1,
     name: "We Care",
-    image: require("../images/html.png"), // Adjust the path as needed
-    info: "A project about care and support."
+    image: require("../images/html.png"),
+    info: "We care is hospital management website that created for booking bed, ordering Medicine. It also provide the bed available in hospital. It also provide information about different Hospital details. The site created using HTML, Bootstrap, java, ASP.net, C#. Handling The backend",
   },
   {
     id: 2,
-    name: "Tech World",
-    image: require("../images/html.png"), // Adjust the path as needed
-    info: "Exploring the latest in tech."
+    name: "Blog Website",
+    image: require("../images/html.png"),
+    info: "Worked remotely as a Full Stack Development Intern at Bharat Intern for one enriching month. Successfully undertook and accomplished challenging tasks, including the creation of a dynamic blog website and implementation of a secure login form using Node.js and MongoDB. Demonstrated proficiency in full-stack development and acquired valuable hands-on experience in web application development. Grateful for the opportunity to contribute to meaningful projects.",
   },
   {
     id: 3,
-    name: "Green Earth",
-    image: require("../images/html.png"), // Adjust the path as needed
-    info: "Environmental awareness and actions."
-  }
+    name: "Real Time Voice Translation",
+    image: require("../images/html.png"),
+    info: "Environmental awareness and actions.",
+  },
 ];
 
 function Project() {
   return (
     <div className="Projectsection">
-      {projects.map(project => (
+      {projects.map((project) => (
         <div className="projectcard" key={project.id}>
           <div className="projectimage">
             <img src={project.image} alt={project.name} />
@@ -35,7 +35,11 @@ function Project() {
             <h3>{project.name}</h3>
             <p>{project.info}</p>
           </div>
-          <button className="projectbutton">Visit<FaExternalLinkAlt/></button>
+          <button className="projectbutton">
+            <a href="/">
+              Visit <FaExternalLinkAlt />
+            </a>
+          </button>
         </div>
       ))}
     </div>
